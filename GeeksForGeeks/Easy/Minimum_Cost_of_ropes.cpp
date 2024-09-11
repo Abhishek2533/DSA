e5561bacb3bd6cd20ef686c4939420ce4c1d5dc0
Abhishek <akrajput1311@gmail.com>
2024-09-11 10:34:00 +0530

    POTD

---
+++ GeeksForGeeks/Easy/Minimum_Cost_of_ropes.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to return the minimum cost of connecting the ropes.
+    long long minCost(vector<long long> &arr)
+    {
+        // Your code here
+
+        // initialize answer
+        long long ans = 0;
+
+        // create min heap
+        priority_queue<long long, vector<long long>, greater<long long>> pq(arr.begin(), arr.end());
+
+        // traverse and get cost
+        while (pq.size() > 1)
+        {
+            long long a = pq.top();
+            pq.pop();
+            long long b = pq.top();
+            pq.pop();
+            ans += a + b;
+            pq.push(a + b);
+        }
+
+        // return answer
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    long long t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        string input;
+        long long num;
+        vector<long long> a;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num)
+        {
+            a.push_back(num);
+        }
+        Solution ob;
+        cout << ob.minCost(a) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
