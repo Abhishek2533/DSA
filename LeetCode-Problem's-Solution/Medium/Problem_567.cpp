8e91c31240a19b0e44b0440c771fb6006a71a1b0
Abhishek <akrajput1311@gmail.com>
2024-10-05 14:57:27 +0530

    POTD

--- LeetCode-Problem's-Solution/Medium/Problem_567.cpp
+++ LeetCode-Problem's-Solution/Medium/Problem_567.cpp
@@ -23,8 +23,6 @@ s1 and s2 consist of lowercase English letters.
 
 */
 
-
-
 // SOURCE CODE
 
 class Solution
@@ -87,5 +85,52 @@ public:
         }
 
         return 0;
+    }
+};
+
+// ANOTHER METHOD
+
+class Solution
+{
+public:
+    bool checkInclusion(string s1, string s2)
+    {
+        unordered_map<char, int> map;
+        for (auto &x : s1)
+            map[x]++;
+
+        int right = 0;
+        int left = 0;
+        int n = s2.length();
+        int required = s1.length();
+
+        while (right < n)
+        {
+            // Decrease frequency of current character
+            if (map[s2[right]] > 0)
+            {
+                required--;
+            }
+            map[s2[right]]--;
+            right++;
+
+            // When the window size equals the length of s1, check if all
+            // characters match
+            if (required == 0)
+                return true;
+
+            // If the window size exceeds the size of s1, shrink the window
+            if (right - left == s1.length())
+            {
+                if (map[s2[left]] >= 0)
+                {
+                    required++;
+                }
+                map[s2[left]]++;
+                left++;
+            }
+        }
+
+        return false;
     }
 };
