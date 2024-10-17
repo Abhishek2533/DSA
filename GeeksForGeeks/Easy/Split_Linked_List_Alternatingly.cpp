d3885d11801751063270553fee05ebf5ba6390a4
Abhishek <akrajput1311@gmail.com>
2024-10-17 22:54:29 +0530

    POTD

---
+++ GeeksForGeeks/Easy/Split_Linked_List_Alternatingly.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // Function to split a linked list into two lists alternately
+    vector<Node*> alternatingSplitList(struct Node* head) {
+        // Your code here
+        if (!head || !head->next) return {head, nullptr};
+
+        Node* first = head;
+        Node* second = head->next;
+        
+        Node* curr1 = first;
+        Node* curr2 = second;
+        Node* curr = second->next;
+        bool alternative = true;
+        
+        while(curr) {
+            if(alternative) {
+                curr1->next = curr;
+                curr1 = curr1->next;
+            } else {
+                curr2->next = curr;
+                curr2 = curr2->next;
+            }
+            curr = curr->next;
+            alternative = !alternative;
+        }
+        
+        curr1->next = NULL;
+        curr2->next = NULL;
+        
+        return {first, second};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        struct Node* head = new Node(arr[0]);
+        struct Node* tail = head;
+
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        vector<Node*> result = ob.alternatingSplitList(head);
+        printList(result[0]);
+        printList(result[1]);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
